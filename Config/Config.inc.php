bf38ff4d683e90f1e03c3c5f24b1ca8881927152
Fermi <shdxcy@gmail.com>
2013-01-02 22:56:53 +0800

    Global Config Modified

--- Config/Config.inc.php
+++ Config/Config.inc.php
@@ -1,12 +1,15 @@
 <?php
-//公用默认路径配置.
+//Global include paths.
 define("BASE_TEMPLATE_DIR",__FILE__.'../Templates');
 define("BASE_SCRIPT_DIR",__FILE__.'../Static/Js');
 define("BASE_STYLE_DIR",__FILE__.'../Static/Style');
 define("BASE_PLUGIN_DIR",__FILE__.'../Plugins');
 define("BASE_CONFIG_DIR",__FILE__.'../Config');
 
-//分支路径配置。
+//Module include paths.
 define("CONTROLLER_MODULE",__FILE__.'../Controller');
 define("LOADER_MODULE",__FILE__.'../Loader');
 define("TOOL_MODULE",__FILE__.'../Tools');
+
+//Tool load options.
+define("DATABASE_MYSQL",'D0');
