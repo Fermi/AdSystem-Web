330691e0b963ddebdc4b2bce05061061abd25b48
FxYCarl <fxycarl@gmail.com>
2013-02-24 16:19:00 +0800

    RenderPath Completed

---
+++ Controller/IndexPage/IndexPage.class.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(dirname(__FILE__)).'/Page/Page.class.php';
+
+class IndexPage extends Page{
+    
+}
--- Controller/Page/Page.class.php
+++ Controller/Page/Page.class.php
@@ -13,15 +13,20 @@ class Page
     private renderedPage;
     //输出PHP页面私有方法。
     public function renderPage($templateIdentifier,$params = array()){
-        if (!defined("TEMPLATE_DIR")){
-            echo "Undefined 'TEMPLATE_DIR'";
+        if ((!defined("TEMPLATES_DIR"))&&(!defined("BASE_TEMPLATES_DIR"))){
+            echo "Undefined 'TEMPLATES_DIR'";
             exit;
         } else {
             if (isset($params)){
                 extract($params);
             }
+            if(defined("TEMPLATES_DIR")){
+                $renderDir = TEMPLATES_DIR.$templateIdentifier;
+            }else if (defined("BASE_TEMPLATES_DIR")){
+                $renderDir = BASE_TEMPLATES_DIR.$templateIdentifier;
+            }
 
-            $this->renderedPage = file_get_contents(TEMPLATE_DIR.$templateIdentifier);
+            $this->renderedPage = file_get_contents($renderDir);
             
             echo $this->renderedPage;
             exit;
@@ -29,15 +34,20 @@ class Page
     } 
     //加载PHP页面脚本私有方法。
     public function loadScript($scriptIdentifier,$params = array()){
-        if (!defined("SCRIPTS_DIR")){
+        if ((!defined("SCRIPTS_DIR"))&&(!defined("BASE_SCRIPTS_DIR"))){
             echo "Undefined 'SCRIPTS_DIR'";
             exit;
         } else {
             if (isset($params)){
                 extract($params);
             }
+            if(defined("SCRIPTS_DIR")){
+                $renderDir = SCRIPTS_DIR.$scriptIdentifier;
+            }else if (defined("BASE_SCRIPTS_DIR")){
+                $renderDir = BASE_SCRIPTS_DIR.$scriptIdentifier;
+            }
             
-            $renderFile = file_get_contents(SCRIPTS_DIR.$scriptIdentifier);
+            $renderFile = file_get_contents($renderDir);
             $excute = '$renderResult = '.$renderFile.';';
 
             @eval($excute);
@@ -49,15 +59,19 @@ class Page
     //本方法区别于loadScript方法用return做返回值的原因是，本方法一般不在页面模板里调用。
     //本方法用于生成特定代码并传递给模板等加载。
     public function formatHelper($pluginIdentifier,$params = array()){
-        if (!defined("PLUGINS_DIR")){
+        if ((!defined("PLUGINS_DIR"))&&(!defined("BASE_PLUGINS_DIR"))){
             echo "Undefined 'PLUGINS_DIR'";
             exit;
         } else {
             if (isset($params)){
                 extract($params);
             }
-            
-            $renderFile = file_get_contents(PLUGINS_DIR.$pluginIdentifier);
+            if(defined("PLUGINS_DIR")){
+                $renderDir = PLUGINS_DIR.$pluginIdentifier;
+            }else if(defined("BASE_PLUGINS_DIR")){
+                $renderDir = BASE_PLUGINS_DIR.$pluginIdentifier;
+            }
+            $renderFile = file_get_contents($renderDir);
             $excute = '$renderResult = '.$renderFile.';';
 
             @eval($excute);
