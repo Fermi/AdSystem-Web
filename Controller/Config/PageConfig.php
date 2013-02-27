f1b8c1c7634df5a4a502be775a4bd79919cf53b5
FxYCarl <fxycarl@gmail.com>
2013-02-27 13:55:04 +0800

    Dir Included

--- Controller/Config/PageConfig.php
+++ Controller/Config/PageConfig.php
@@ -4,6 +4,7 @@ require_once dirname(__FILE__).'/Config.php'
 define('BASE_TEMPLATES_DIR',ROOT_DIR.'/Templates');
 define('BASE_SCRIPTS_DIR',ROOT_DIR.'/Statics/Scripts');
 define('BASE_PLUGINS_DIR',ROOT_DIR.'/Plugins');
+define('BASE_WIDGET_DIR',ROOT_DIR.'/Widgets')
 
 
 
