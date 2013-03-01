288f7208152ca0f30dd96fbd539ab1f44bd537e7
FxYCarl <fxycarl@gmail.com>
2013-03-01 12:49:40 +0800

    Page Config Bug Fixed

--- Controller/Config/PageConfig.php
+++ Controller/Config/PageConfig.php
@@ -1,10 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/Config.php';
 //Options.
-define("BASE_TEMPLATES_DIR",ROOT_DIR.'/Templates');
-define("BASE_SCRIPTS_DIR",ROOT_DIR.'/Statics/Scripts');
-define("BASE_PLUGINS_DIR",ROOT_DIR.'/Plugins');
-define("BASE_WIDGET_DIR",ROOT_DIR.'/Widgets');
+
 
 
 
