846f460eff5a5b9d1d9445b72f55e1bbbb812ec6
FxYCarl <fxycarl@gmail.com>
2013-04-07 23:27:55 +0800

    Downloader finished

--- Tool/FileOperation/FileDownloader/FileDownloader.class.php
+++ Tool/FileOperation/FileDownloader/FileDownloader.class.php
@@ -2,7 +2,27 @@
 require_once dirname(dirname(dirname(__FILE__))).'/Config/FileOperation.php';
 
 class FileDownloader{
-    public function downloadFiles($source_path = NULL){
+    public function downloadFiles($file,$source_path = NULL){
+        if(!empty($source_path)){
+            $file_path = FILE_PREFIX.$source_path;
+        } else {
+            $file_path = FILE_PREFIX;
+        }
         
+        if(!empty($file)){
+            if(file_exists($file_path,$file)){
+                header("Content-Type: application/octet-stream");
+                header("Content-Length: ".filesize($file_path.$file));
+                $fp = fopen($file_path.$file,"r");
+                echo fread($fp,filesize($file_path.$file));
+                fclose($fp);
+                return json_encode(array("Error"=>""));
+            } else {
+                return json_encode(array("Error"=>"File does not exist!"));
+            }
+        } else {
+            return json_encode(array("Error"=>"Filename must be wrong!"));
+        }
+
     }
-}    
+}      
