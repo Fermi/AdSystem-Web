9b53ba2274542b8e44d318c2ece01e7671aa59ac
FxYCarl <fxycarl@gmail.com>
2013-04-08 01:44:01 +0800

    Downloader planed

--- Tool/FileOperation/FileDownloader/FileDownloader.class.php
+++ Tool/FileOperation/FileDownloader/FileDownloader.class.php
@@ -25,4 +25,8 @@ class FileDownloader{
         }
 
     }
+
+    public function getFileNames($filter){
+        
+    }
 }      
--- Tool/FileOperation/FileDownloader/FileDownloader.php
+++ Tool/FileOperation/FileDownloader/FileDownloader.php
@@ -8,6 +8,13 @@ if(ParamManager::getGet("Filter")){
 if(ParamManager::getGet("File")){
     $file = ParamManager::getGet("File");
 }
+if(ParamManager::getGet("GetFileName")){
+    $get_file_name = ParamManager::getGet("GetFileName");
+}
 
-new FileDownloader()->downloadFiles($file,$filter);
-
+if($file||$filter){
+    new FileDownloader()->downloadFiles($file,$filter);
+}
+if($get_file_name){
+    new FileDownloader()->getFileNames($filter);
+}
