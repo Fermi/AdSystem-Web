0e4cc8351e4b597eaa83087f416a6fe7926efda3
FxYCarl <fxycarl@gmail.com>
2013-04-09 22:33:28 +0800

    Config fixed

--- Tool/FileOperation/FileUploader/FileUploader.php
+++ Tool/FileOperation/FileUploader/FileUploader.php
@@ -1,5 +1,7 @@
 <?php
-require_once dirname(__FILE__)."/FileUploader.class.php";
+//Include File Config.
+//require_once
+require_once FILE_UPLOADER."/FileUploader.class.php";
 require_once TOOL_MODULE."/Param/ParamManager.class.php";
 
 if(ParamManager::getGet("Filter")){
