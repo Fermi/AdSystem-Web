56de7a83c57fa0070f42de8a7cda8e7e17a1e7b4
FxYCarl <fxycarl@gmail.com>
2013-04-28 06:24:47 +0800

    session_start added

--- Lander/Page/AjaxLander.php
+++ Lander/Page/AjaxLander.php
@@ -5,5 +5,8 @@ require_once TOOL_MODULE.'/Param/ParamManager.class.php';
 require_once LOADER_MODULE.'/Page/PageLoader.class.php';
 
 $loader = new PageLoader();
-
-$loader->AjaxLoadPage(ParamManager::getGet('module_name'),ParamManager::getGet('page_name'));
+if(session_start()){
+    $loader->AjaxLoadPage(ParamManager::getGet('module_name'),ParamManager::getGet('page_name'));
+} else {
+    echo "用户Session出现错误!";
+}
--- Lander/Page/IndexLander.php
+++ Lander/Page/IndexLander.php
@@ -5,4 +5,8 @@ require_once TOOL_MODULE.'/Param/ParamManager.class.php';
 require_once LOADER_MODULE.'/Page/PageLoader.class.php';
 
 $loader=new PageLoader();
-$loader->LoadPage(ParamManager::getGet('module_name'),ParamManager::getGet('page_name'));
+if(session_start()){
+    $loader->LoadPage(ParamManager::getGet('module_name'),ParamManager::getGet('page_name'));
+} else {
+    echo "用户Session出现错误!";
+}
