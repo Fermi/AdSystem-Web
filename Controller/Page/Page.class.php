21678a52cc9e2a758759c8b0134816cc832e633e
FxYCarl <fxycarl@gmail.com>
2013-05-09 07:05:49 +0800

    widget system added

--- Controller/Page/Page.class.php
+++ Controller/Page/Page.class.php
@@ -6,19 +6,19 @@
 * Date:     Dec 10,2012
 */
 require_once dirname(dirname(__FILE__)).'/Config/PageConfig.php';
-require_once BASE_WIDGETS_DIR.'WidgetManager/WidgetsManager.class.php';
-require_once BASE_PLUGINS_DIR.'PluginManager/PluginManager.class.php';
+require_once BASE_WIDGETS_DIR.'WidgetManager/WidgetManager.class.php';
+//require_once BASE_PLUGINS_DIR.'PluginManager/PluginManager.class.php';
 
 class Page {
     //私有变量
     //页面里有的组件.
     private $widgets;
-    private $plugins;
+    //private $plugins;
 
     //构造方法.
     public function __construct(){
         $this->widgets = new WidgetManager();
-        $this->plugins = new PluginManager();
+        //$this->plugins = new PluginManager();
     }
     //输出PHP页面公有方法。
     public function renderPage($templateIdentifier,$params){
@@ -71,13 +71,13 @@ class Page {
         echo $widgets->getScripts();
     }
     //控制器中注册插件.
-    public function registPlugin($name,$params){
-        return $plugins->regist($name,$params);
-    }
+    //public function registPlugin($name,$params){
+    //    return $plugins->regist($name,$params);
+    //}
     //页面导出插件结果.
-    public function exportPluginResult($name){
-        echo $plugins->getResult($name);
-    }
+    //public function exportPluginResult($name){
+    //    echo $plugins->getResult($name);
+    //}
     //继承控制器基类的控制器类需要覆盖的默认调用方法。
     //public function initPage(){
        //$this->renderPage();
